e230e8b6e69b7d187b2a4f8f6ec64442c48f6aa9
Bogdan Tatarov <bogdan.tatarov@gmail.com>
2018-11-10 21:26:13 +0200

    Support for macos and linux

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-caffeinate -i python mariobot.py
+if [ "$(uname)" == "Darwin" ]; then
+    export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+    caffeinate -i python mariobot.py
+else
+    python mariobot.py
+fi
